a411d56082fdbf1738307dee1e023487f40fcb45
Dan Wasyluk <dwasyluk@gmail.com>
2014-12-27 03:09:54 -0500

    syscoinService working with CORS
    Syscoin service now working with CORS and builtin HTTP.

--- app/allitems/allitems.controllers.js
+++ app/allitems/allitems.controllers.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('blockmarket.allitems.controllers', ['syscoin.services'])
-    .controller('AllItemsCtrl', ['$rootScope', '$scope', '$route', 'syscoinService', 'syscoinService2', function ($rootScope, $scope, $route, syscoinService, syscoinService2) {
-        $rootScope.activeView = 'items'; //sets the style for nav
+    .controller('AllItemsCtrl', ['$rootScope', '$scope', '$route', 'syscoinService', function ($rootScope, $scope, $route, syscoinService) {
+        //$rootScope.activeView = 'items'; //sets the style for nav
 
         //TODO: refactor to us a .all() request
         //fetch all items
@@ -34,9 +34,12 @@ angular.module('blockmarket.allitems.controllers', ['syscoin.services'])
         }); */
 
         //dummy call to syscoin service for now
-       // var request = syscoinService.getInfo();
+        var request = syscoinService.getInfo();
+        request.then(function(response) {
+            console.log("got info!");
+        });
 
-        syscoinService2.getInfo({}, {headers: {'X-ACL': 'x@y.z'}}).
+       /* syscoinService2.getInfo({}, {headers: {}}).
             success(function(data) { console.log("success on getinfo!", data); }).
-            error(function(error) { console.log("error on getinfo!", error); });
+            error(function(error) { console.log("error on getinfo!", error); }); */
     }]);
--- components/syscoin.services.js
+++ components/syscoin.services.js
@@ -1,13 +1,10 @@
-angular.module('syscoin.services', ['blockmarket.appconfig', 'jsonrpc'])
-    .service('syscoinService' ['Base64', '$http', '$q', 'RPC_CONFIG', 'syscoinService2', function(Base64, $http, $q, RPC_CONFIG, jsonrpc) {
+angular.module('syscoin.services', ['blockmarket.appconfig'])
+    .service('syscoinService', ['Base64', '$http', '$q', 'RPC_CONFIG', function(Base64, $http, $q, RPC_CONFIG) {
 
-        var rpcUrl = RPC_CONFIG.HOST + ":" + RPC_CONFIG.PORT + "/";
+        var rpcUrl = "http://" + RPC_CONFIG.HOST + ":" + RPC_CONFIG.PORT + "/";
         var authdata = Base64.encode(RPC_CONFIG.USERNAME + ':' + RPC_CONFIG.PASSWORD);
 
-        var service = jsonrpc.newService('syscoinsvc');
-        this.getInfo2 = service.createMethod('getInfo');
-
-        $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
+        $http.defaults.headers.common['Authorization'] = getAuthHeader(); // jshint ignore:line
 
         function getAuthHeader() {
             return  'Basic ' + authdata;
@@ -18,22 +15,16 @@ angular.module('syscoin.services', ['blockmarket.appconfig', 'jsonrpc'])
             var request = $http({
                 method: "post",
                 url: rpcUrl,
-                headers: {
-                    "X-Requested-With" : "XMLHttpRequest"
-                },
                 data: {
                     "method" : "getinfo"
-                }
+                },
+                params: {}
             });
-
-            locationService.save({lat: 22, long: 33}, {headers: {'X-ACL': 'x@y.z'}}).
-                success(function(data) {}).
-                error(function(error) {})
 
             return( request );
         }
 
-        function offerNew( category, title, quantity, price, description ) {
+        /*function offerNew( category, title, quantity, price, description ) {
             console.log("offerNew(" + category + ", " + title + ", " + quantity + ", " + price + ", " + description + ")");
             var request = $http({
                 method: "post",
@@ -141,19 +132,103 @@ angular.module('syscoin.services', ['blockmarket.appconfig', 'jsonrpc'])
             });
 
             return( request );
-        }
+        } */
 
 
         // Return public API.
         return {
-            getInfo: getInfo,
-            offerNew: offerNew,
+            getInfo: getInfo
+            /*offerNew: offerNew,
             listTransactions: listTransactions,
             offerList: offerList,
             offerInfo: offerInfo,
             certissuerList: certissuerList,
             aliasList: aliasList,
             getRawTransaction: getRawTransaction,
-            decodeRawTransaction: decodeRawTransaction
+            decodeRawTransaction: decodeRawTransaction*/
         };
-    }])
+    }]).factory('Base64', function () {
+        /* jshint ignore:start */
+
+        var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+        return {
+            encode: function (input) {
+                var output = "";
+                var chr1, chr2, chr3 = "";
+                var enc1, enc2, enc3, enc4 = "";
+                var i = 0;
+
+                do {
+                    chr1 = input.charCodeAt(i++);
+                    chr2 = input.charCodeAt(i++);
+                    chr3 = input.charCodeAt(i++);
+
+                    enc1 = chr1 >> 2;
+                    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                    enc4 = chr3 & 63;
+
+                    if (isNaN(chr2)) {
+                        enc3 = enc4 = 64;
+                    } else if (isNaN(chr3)) {
+                        enc4 = 64;
+                    }
+
+                    output = output +
+                        keyStr.charAt(enc1) +
+                        keyStr.charAt(enc2) +
+                        keyStr.charAt(enc3) +
+                        keyStr.charAt(enc4);
+                    chr1 = chr2 = chr3 = "";
+                    enc1 = enc2 = enc3 = enc4 = "";
+                } while (i < input.length);
+
+                return output;
+            },
+
+            decode: function (input) {
+                var output = "";
+                var chr1, chr2, chr3 = "";
+                var enc1, enc2, enc3, enc4 = "";
+                var i = 0;
+
+                // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+                var base64test = /[^A-Za-z0-9\+\/\=]/g;
+                if (base64test.exec(input)) {
+                    window.alert("There were invalid base64 characters in the input text.\n" +
+                        "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                        "Expect errors in decoding.");
+                }
+                input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+                do {
+                    enc1 = keyStr.indexOf(input.charAt(i++));
+                    enc2 = keyStr.indexOf(input.charAt(i++));
+                    enc3 = keyStr.indexOf(input.charAt(i++));
+                    enc4 = keyStr.indexOf(input.charAt(i++));
+
+                    chr1 = (enc1 << 2) | (enc2 >> 4);
+                    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                    chr3 = ((enc3 & 3) << 6) | enc4;
+
+                    output = output + String.fromCharCode(chr1);
+
+                    if (enc3 != 64) {
+                        output = output + String.fromCharCode(chr2);
+                    }
+                    if (enc4 != 64) {
+                        output = output + String.fromCharCode(chr3);
+                    }
+
+                    chr1 = chr2 = chr3 = "";
+                    enc1 = enc2 = enc3 = enc4 = "";
+
+                } while (i < input.length);
+
+                return output;
+            }
+        };
+
+        /* jshint ignore:end */
+    });
