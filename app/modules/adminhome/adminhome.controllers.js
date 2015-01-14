341e71ac7a5bc018f456ec27ab19005ae548d08d
Dan Wasyluk <dwasyluk@gmail.com>
2015-01-14 00:08:31 -0500

    Building out admin view
    * Admin modal added, needs skinning

--- app/modules/adminhome/adminhome.controllers.js
+++ app/modules/adminhome/adminhome.controllers.js
@@ -1,7 +1,27 @@
 'use strict';
 
-angular.module('adminhome.controllers', ['blockmarket.services', 'angular-syscoin'])
-    .controller('AdminHomeCtrl', ['$rootScope', '$scope', '$q', 'blockmarketService', 'syscoinService', function ($rootScope, $scope, $q, blockmarketService, syscoinService) {
+angular.module('adminhome.controllers', ['blockmarket.services', 'angular-syscoin', 'ui.bootstrap'])
+    .controller('AdminHomeCtrl', ['$rootScope', '$scope', '$q', 'blockmarketService', 'syscoinService', '$modal', function ($rootScope, $scope, $q, blockmarketService, syscoinService, $modal) {
         $rootScope.activeView = 'admin'; //sets the style for nav
 
+        $scope.launchModal = function(modalType) {
+            var modalInstance = $modal.open({
+                templateUrl: 'app/modules/adminhome/partials/addItemModal.tpl.html',
+                controller: 'AddItemCtrl'
+            });
+        }
+
+    }])
+    .controller('AddItemCtrl', ['$rootScope', '$scope', '$q', 'blockmarketService', 'syscoinService', '$modalInstance', function ($rootScope, $scope, $q, blockmarketService, syscoinService, $modalInstance) {
+        $rootScope.activeView = 'admin'; //sets the style for nav
+
+        $scope.master = {};
+
+        $scope.addItem = function(item) {
+            $scope.master = angular.copy(item);
+        };
+
+        $scope.cancel = function() {
+            $modalInstance.dismiss('cancel');
+        }
     }]);
