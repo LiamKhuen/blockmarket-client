206e9fe18726874aba1960524c039fdfd813f345
Dan Wasyluk <dwasyluk@gmail.com>
2015-01-25 15:38:06 -0500

    Removed misplaced controller

--- app/shared/controllers/buyitem.controllers.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('adminhome.controllers', ['blockmarket.services', 'ui.bootstrap'])
-    .controller('BuyItemCtrl', ['$rootScope', '$scope', '$log', '$modalInstance', function ($rootScope, $scope, $log, $modalInstance) {
-        $scope.cancel = function() {
-            $modalInstance.dismiss('cancel');
-        }
-    }]);
